a2cc9a51bc912816f390a66b31e578a4385b43d4
hassan alhassaini <hassandak1d@hotmail.com>
2021-09-21 19:37:06 +0300

    done

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+   console.log (`Hello ${name}`)
 }
 
 /**
@@ -20,7 +20,12 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+ if ( n % 2 === 0){
+  return false
+ }
+ else {
+   return true
+ }
 }
 
 /**
@@ -33,8 +38,15 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+if (n % 2 === 0) {
+  return n / 2
+} else {
+  return (n-1) / 2
 }
+}
+
+
+
 
 /**
  * squareOrDouble(n):
@@ -47,7 +59,13 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
+  if  (n % 2 === 0){
+    return n * 2
+  } else { 
+    return n * n
+  
+    }
+  }
   // Your code here
-}
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
